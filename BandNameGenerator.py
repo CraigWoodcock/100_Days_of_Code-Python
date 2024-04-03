ebe67f353bfeb05bd07dc79b53a4084d47fdd96b
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-04-03 12:42:20 +0100

    starting 100 days of code - python to refresh previous learning

---
+++ BandNameGenerator.py
@@ -0,0 +1,5 @@
+# print("welcome to the band name generator")
+# my_town = input("what is the name of the town you grew up in? \n")
+# my_pet = input("what is the name of your pet? \n" )
+# print("your band name could be "+my_town+" "+my_pet)
+
