abe09269907fa84c62c7f48281f6ef9ce60fe55b
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-04-04 15:33:41 +0100

    data types

--- Day2.py
+++ Day2.py
@@ -5,3 +5,23 @@
 #print certain index of a string (subscripting)
 print("Hello"[2])
 
+#concatenation
+print("123"+"456")
+
+# integer
+
+print(123+456) #returns the sum of these integers.
+
+print(123_465_789) # underscores will be removed.
+
+#float - any number that has a decimal place:
+
+print(3.14159)
+
+#Boolean - True/False
+
+True
+False
+
+
+
