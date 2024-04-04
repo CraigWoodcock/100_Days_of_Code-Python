afe9bdd85f224ad68e3cc0f29cc99d46a98646be
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-04-04 16:21:08 +0100

    adding practice area

---
+++ practice.py
@@ -0,0 +1,10 @@
+
+two_digit_number = input("two digit number ")
+
+print(type(two_digit_number))
+
+a = int(two_digit_number[0])
+b = int(two_digit_number[1])
+
+two_digit_number = a+b
+print(two_digit_number)
