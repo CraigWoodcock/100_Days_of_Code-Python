c42fb6105587c5df73b6e77cc68fe500144f23f8
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-04-05 15:00:29 +0100

    completed tip calculator

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -3,3 +3,9 @@
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
 
+print("Welcome to the tip calculator!")
+total_bill = float(input("what was the total of the bill? "))
+tip = int(input("how much would you like to tip (%)? 10, 12 or 15 "))
+people = int(input("how many people should the bill be split between? "))
+total_inc_tip = round((((tip/100)+1)*total_bill)/people,2)
+print(total_inc_tip)
