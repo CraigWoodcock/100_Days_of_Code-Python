e4f955aaa1a9c09e0dab84e5cc4b8d7e87885fdc
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-04-05 12:30:04 +0100

    rounding numbers

--- Day2.py
+++ Day2.py
@@ -24,7 +24,7 @@ True
 False
 
 # type
-length = len(input("what is your name? "))
+length = len("craig")
 print(length)
 print(type(length))
 
@@ -41,6 +41,24 @@ length_to_string = str(length)
 #now we can run the line above, using the new variable(length_to_string):
 
 print("your name has " + length_to_string + " characters")
+
+
+
+
+#rounding numbers
+
+#use the round() function to round to the nearest whole number:
+print(round(8/3))
+
+##use the floor operator to round down to the nearest whole number:
+
+print(round(8//3))
+
+#round the equation to x decimal places:
+print(round(8/3,2))
+
+#round a number to x decimal places
+print(round(2.675425625,2))
 
 
 
