d1d8f9f03fb84e22a96093f07483a98014606d40
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-04-07 09:12:37 +0100

    completed tip calc

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -5,7 +5,7 @@
 
 print("Welcome to the tip calculator!")
 total_bill = float(input("what was the total of the bill? "))
-tip = int(input("how much would you like to tip (%)? 10, 12 or 15 "))
+tip = float(input("how much would you like to tip (%)? 10, 12 or 15 "))
 people = int(input("how many people should the bill be split between? "))
 total_inc_tip = round((((tip/100)+1)*total_bill)/people,2) # divide the tip by 100 and +1, multiply this figure by the total bill to get the total including the tip, divide this by the amount of people and finally round it off to 2 decimal places 
 print(f"Each person should pay: £{total_inc_tip}")
