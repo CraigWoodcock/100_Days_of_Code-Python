942efff0cfb843e2e162a50c79f9cb38889bb6bd
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-04-07 09:27:29 +0100

    added and completed rollercoaster height checker

---
+++ HeightChecker.py
@@ -0,0 +1,15 @@
+
+# def main():
+#     height_checker()
+
+# def height_checker():
+print("Welcome to the Rollercoaster!!")
+height = int(input("What is your height in cm?: \n"))
+if height <= 119:
+    print("You aren't tall enough to ride this RollerCoaster.")
+else:
+    print("You are tall enough to ride the RollerCoaster.")
+
+
+# if __name__ == "__main__":
+#     main()
